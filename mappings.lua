f7dbc5832d52d7bd948ec0394187b91eb6709e97
ansidev <ansidev@gmail.com>
2024-12-25 11:01:56 +0700

    fix: upgrade mappings

--- mappings.lua
+++ mappings.lua
@@ -4,45 +4,38 @@ function M.load_conduct_mappings()
   return {
     n = {
       -- Project
-      ["<leader>p"] = {
-        name = "Project",
-        a = {
-          function()
-            local FvInput = require("ui.input")
-            local input = FvInput({
-              title = "Project name",
-              default_value = "",
-              on_submit = function(value)
-                vim.cmd("ConductNewProject " .. value)
-              end,
-            })
+      { "<leader>p", group = "Project" },
+      {
+        "<leader>pa",
+        function()
+          local FvInput = require("ui.input")
+          local input = FvInput({
+            title = "Project name",
+            default_value = "",
+            on_submit = function(value)
+              vim.cmd("ConductNewProject " .. value)
+            end,
+          })
 
-            input:mount()
-          end,
-          "Add project",
-        },
-        d = {
-          "<Cmd>ConductDeleteProject<CR>",
-          "Delete the current project",
-        },
-        l = {
-          "<Cmd>Telescope conduct projects<CR>",
-          "List projects",
-        },
+          input:mount()
+        end,
+        desc = "Add project",
       },
+      { "<leader>pd", "<Cmd>ConductDeleteProject<CR>", desc = "Delete the current project" },
+      { "<leader>pl", "<Cmd>Telescope conduct projects<CR>", desc = "List projects" },
     }
   }
 end
 
 local window_mappings = {
-  ["<A-h>"] = { "<Cmd>lua require('smart-splits').move_cursor_up()<CR>", "Go to the up window" },
-  ["<A-j>"] = { "<Cmd>lua require('smart-splits').move_cursor_down()<CR>", "Go to the down window" },
-  ["<A-k>"] = { "<Cmd>lua require('smart-splits').move_cursor_left()<CR>", "Go to the left window" },
-  ["<A-l>"] = { "<Cmd>lua require('smart-splits').move_cursor_right()<CR>", "Go to the right window" },
-  ["<A-Up>"] = { "<Cmd>lua require('smart-splits').move_cursor_up()<CR>", "Go to the up window" },
-  ["<A-Down>"] = { "<Cmd>lua require('smart-splits').move_cursor_down()<CR>", "Go to the down window" },
-  ["<A-Left>"] = { "<Cmd>lua require('smart-splits').move_cursor_left()<CR>", "Go to the left window" },
-  ["<A-Right>"] = { "<Cmd>lua require('smart-splits').move_cursor_right()<CR>", "Go to the right window" },
+  { "<A-h>", "<Cmd>lua require('smart-splits').move_cursor_up()<CR>", desc = "Go to the up window" },
+  { "<A-j>", "<Cmd>lua require('smart-splits').move_cursor_down()<CR>", desc = "Go to the down window" },
+  { "<A-k>", "<Cmd>lua require('smart-splits').move_cursor_left()<CR>", desc = "Go to the left window" },
+  { "<A-l>", "<Cmd>lua require('smart-splits').move_cursor_right()<CR>", desc = "Go to the right window" },
+  { "<A-Up>", "<Cmd>lua require('smart-splits').move_cursor_up()<CR>", desc = "Go to the up window" },
+  { "<A-Down>", "<Cmd>lua require('smart-splits').move_cursor_down()<CR>", desc = "Go to the down window" },
+  { "<A-Left>", "<Cmd>lua require('smart-splits').move_cursor_left()<CR>", desc = "Go to the left window" },
+  { "<A-Right>", "<Cmd>lua require('smart-splits').move_cursor_right()<CR>", desc = "Go to the right window" },
 }
 
 function M.load_smart_splits_mappings()
