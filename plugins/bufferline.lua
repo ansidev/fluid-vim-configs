323254363a6e3d0c180c12633d3362eb97272b54
ansidev <ansidev@gmail.com>
2024-12-26 19:13:45 +0700

    chore(plugin): update bufferline configs

--- plugins/bufferline.lua
+++ plugins/bufferline.lua
@@ -3,9 +3,9 @@ return {
   options = {
     builtin_configs = {
       options = {
-        separator_style = "slope",
+        -- separator_style = "slope",
         -- separator_style = "padded_slope",
-        -- separator_style = { "" .. " ", "" .. " ", true }
+        separator_style = { "" .. " ", "" .. " ", true }
       },
     },
   },
