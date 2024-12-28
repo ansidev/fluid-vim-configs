7be2ed9988b3136be82dc56525a727fb08fd05e3
ansidev <ansidev@gmail.com>
2024-12-28 21:49:52 +0700

    chore: remove unused variable

--- mappings.lua
+++ mappings.lua
@@ -1,16 +1,5 @@
 local M = {}
 
-local window_mappings = {
-  { "<A-h>", "<Cmd>lua require('smart-splits').move_cursor_up()<CR>", desc = "Go to the up window" },
-  { "<A-j>", "<Cmd>lua require('smart-splits').move_cursor_down()<CR>", desc = "Go to the down window" },
-  { "<A-k>", "<Cmd>lua require('smart-splits').move_cursor_left()<CR>", desc = "Go to the left window" },
-  { "<A-l>", "<Cmd>lua require('smart-splits').move_cursor_right()<CR>", desc = "Go to the right window" },
-  { "<A-Up>", "<Cmd>lua require('smart-splits').move_cursor_up()<CR>", desc = "Go to the up window" },
-  { "<A-Down>", "<Cmd>lua require('smart-splits').move_cursor_down()<CR>", desc = "Go to the down window" },
-  { "<A-Left>", "<Cmd>lua require('smart-splits').move_cursor_left()<CR>", desc = "Go to the left window" },
-  { "<A-Right>", "<Cmd>lua require('smart-splits').move_cursor_right()<CR>", desc = "Go to the right window" },
-}
-
 function M.load_smart_splits_mappings()
   return {
     n = {
